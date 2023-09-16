4a2bb15a1dd9b07e1ed5328cc38ecaae4c808980
iikunjoker <144687190+iikunjoker@users.noreply.github.com>
2023-09-16 15:46:59 +0800

    Add files via upload

---
+++ 3121005096/PaperCheck/src/main/java/com/Jarvis/utils/ShortStringException.java
@@ -0,0 +1,19 @@
+package com.Jarvis.utils;
+
+public class ShortStringException extends Exception {
+    public ShortStringException() {
+        super();
+    }
+
+    public ShortStringException(String message) {
+        super(message);
+    }
+
+    public ShortStringException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ShortStringException(Throwable cause) {
+        super(cause);
+    }
+}
